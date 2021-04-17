73bc19d913d2e3383a76ec516a00882b008f6275
Lucie20 <60733340+Lucie20@users.noreply.github.com>
2021-04-17 18:48:54 +0200

    ukol 6 hotovo

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 // Použij výpis do konzole prohlížeče pomocí console.log('text')
 // Konzoli otevřeš v Chrome stiskem F12 (na Windows) nebo Command-Option-I (na Macu)
 
-
 // První příklad - vypiš vzestupně čísla od 0 do 10
 console.log('První příklad - vzestupná řada 0 - 10');
 
@@ -9,7 +8,15 @@ console.log('První příklad - vzestupná řada 0 - 10');
 
 console.log('-------------------');
 
-
+// for (i = 0; i <= 10; i++) {
+//   console.log('for' + i);
+// }
+ 
+let i = 0;
+while (i <= 10) {
+  console.log ('while' + i);
+  i++;
+}
 
 // Druhý příklad - zkopíruj předchozí příklad
 // a vyzkoušej, jaký je rozdíl mezi <= a <
@@ -21,7 +28,15 @@ console.log('Druhý příklad - rozdíl mezi <= a <');
 
 console.log('-------------------');
 
-
+// for (i = 0; i < 10; i++) {
+//   console.log('for' + i);
+// }
+ 
+let j = 0;
+while (j < 10) {
+  console.log ('while' + j);
+  j++;
+}
 
 // Třetí příklad - vypiš sestupně čísla od 10 do 0
 console.log('Třetí příklad - sestupná řada 10 - 0');
@@ -29,3 +44,13 @@ console.log('Třetí příklad - sestupná řada 10 - 0');
 //TADY NAPIS CYKLUS
 
 console.log('-------------------');
+
+// for (let i = 10; i >= 0; i = i - 1) {
+//   console.log('for ' + i);
+// }
+
+let k = 10;
+while (k >= 0) {
+  console.log('while ' + k);
+  k--;
+}
